e6d0a318df7d45d253507014d396948047d0e015
Jason Hibbs <jason@jasonhibbs.co.uk>
2014-11-07 22:35:54 +0000

    2.0.7

--- resources/js/plugins.js
+++ resources/js/plugins.js
@@ -37,7 +37,6 @@
 })( this );
 
 
-
 // Keep relative height on video iframes
 $(function() {
     
@@ -64,4 +63,144 @@ $(function() {
   
   }).resize();
 
+});
+
+
+// Toggle ARIA /////////////////////////////////////////////////////
+// Version: 0.1
+$.fn.toggleARIA = function(opts) {
+
+  var $el = $(this);
+  var el_id = $el.attr('id') ? $el.attr('id') : 'element';
+  
+  var defaults = {
+    toggle: '[aria-controls="' + el_id + '"]',
+    binds: 'click',
+    onExpand: '',
+    onCollapse: '',
+    tidySelf: false,
+    tidyOthers: '',
+  };
+
+  var options = $.extend( {}, $.fn.toggleARIA.defaults, opts );
+
+  var $toggle = (options.toggle) ? $(options.toggle) : $(defaults.toggle);
+  var binds = (options.binds) ? options.binds : defaults.binds;
+  
+  var $html = $('html');
+  var $window = $(window);
+  
+  var collapsed_class = el_id + '_collapsed';
+  var expanded_class = el_id + '_expanded';
+
+  // Functions
+  function collapse(el) {
+    el.attr('aria-expanded', 'false');
+    $html.removeClass(expanded_class);
+    $html.addClass(collapsed_class);
+    
+    if (typeof options.onCollapse === 'function') {
+      options.onCollapse.call(this);
+    }
+  }
+  
+  function expand(el) {
+    el.attr('aria-expanded', 'true');
+    $html.removeClass(collapsed_class);
+    $html.addClass(expanded_class);
+    
+    if (options.tidyOthers) {
+      var others = options.tidyOthers.split(',');
+      
+      $.each(others, function(i, c) {
+        var other = c.trim() + '_expanded';
+        if ($html.hasClass(other) === true) {
+          // Now collapse 'c'
+        }
+      });
+    }
+    
+    if (typeof options.onExpand === 'function') {
+      options.onExpand.call(this);
+    }
+  }
+  
+  function toggler(el) {
+    if ( el.attr('aria-expanded') === 'true' ) {
+      collapse(el);
+    } else {
+      expand(el);
+    }
+  }
+  
+  // Setup
+  if ($toggle.is(':hidden')) {
+    expand($el);
+  } else {
+    collapse($el);
+  }
+  
+  // Binds
+  if (binds === 'hover') {
+    // This hover option is not ready for the real world
+    if ($el.children($toggle)) {      
+      $toggle.mouseenter(function() {
+        expand($el);
+      });
+      
+      $el.mouseleave(function() {
+        collapse($el);
+      });
+      
+      $toggle.click(function() {
+        collapse($el);
+      });
+    
+    } else {
+      $toggle.hover(function() {
+        toggler($el);
+      }); 
+    }
+
+    // Not actually sure why this works…    
+    $toggle.on('touchend', function() {
+      expand($el);
+      $toggle.off('mouseenter');
+    });
+    
+  } else {
+    // Default to click
+    $toggle.click(function() {
+      toggler($el);
+    });
+    
+    if (options.tidySelf) {
+      $el.mouseleave(function() {
+        collapse($el);
+      });
+    } 
+  }
+  
+  // Might be a better way to do this…
+  if (options.tidySelf) {
+    $('.content').on('mouseup touchstart', function() {
+      collapse($el);
+    });
+  }
+
+  $window.resize(function() {
+    if ($toggle.is(':hidden')) {
+      expand($el);
+    }
+  });
+};
+
+
+// Find existing ARIA controls in the page
+$(document).ready(function() {
+  var presets = $('[aria-controls]');
+  presets.each(function() {
+    var el = $(this).attr('aria-controls');
+    $('#' + el).toggleARIA();
+  });
 });
